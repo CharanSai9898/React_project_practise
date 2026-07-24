e68f6b25a956581c913e72eb62eaa0ab01461ad1
Koppisetti Charan Sai <koppisetticharansai@gmail.com>
2026-07-24 11:51:33 +0530

    added acordion project

---
+++ question/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { data } from "./data";
+
+
+import Questions from "./Questions";
+
+const App = () => {
+  const [questions,setQuestions] = useState(data);
+
+  return (
+    <main>
+      <Questions questions={questions} />
+    </main>
+  );
+};
+
+export default App;
